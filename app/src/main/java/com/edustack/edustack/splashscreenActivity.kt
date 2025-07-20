d76a08a9e5fc24ed862ed6bb19e50190c5083eb8
sachithasamadhib <sachithasbandara6@gmail.com>
2025-07-21 00:27:30 +0530

    add files

---
+++ app/src/main/java/com/edustack/edustack/splashscreenActivity.kt
@@ -0,0 +1,33 @@
+package com.edustack.edustack
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splashscreenActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splashscreen)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById( R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 👇 Add this part
+        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
